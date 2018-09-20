179fd2f8deb9cafa46faed3e4487c57ec678dacb
bao <camhoangbao@gmail.com>
2018-09-20 16:10:21 +0700

    Finish For_loop

---
+++ src/bao/dev/For_loop.java
@@ -0,0 +1,11 @@
+package bao.dev;
+
+public class For_loop {
+	public static void main(String[] args) {
+	int tong = 0;
+	for (int value = 1; value <=100; value++) {
+	tong = tong + value;
+	}
+	System.out.println("Tong la:" + tong);
+	}
+} 
