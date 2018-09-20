5c71710d15a63cb31eb6db7adeb3a2a4e7cbcd67
bao <camhoangbao@gmail.com>
2018-09-20 16:12:31 +0700

    Finish While_loop

---
+++ src/bao/dev/While_loop.java
@@ -0,0 +1,15 @@
+	package bao.dev;
+
+public class While_loop {
+	public static void main(String[] agrs) {
+		int i = 0;
+		int sum = 0;
+		while (i <= 100) {
+		sum = sum +i;
+		i++;
+		}
+		System.out.println("Tong 100 so la: " +sum);
+
+		}
+}
+
