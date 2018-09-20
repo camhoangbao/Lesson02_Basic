6fe00ba2cbdf8397cc437b495ba2d9bdf06d2e07
bao <camhoangbao@gmail.com>
2018-09-20 16:00:18 +0700

    Finish operator_basic

---
+++ src/bao/dev/Basic.java
@@ -0,0 +1,13 @@
+package bao.dev;
+
+public class Basic {
+	public static void main(String[] agrs) {
+		int a = 5;
+		int b = 8;
+		System.out.println("Tong cua 2 so a va b la: "+(a+b));
+		System.out.println("Hieu cua 2 so a va b la:" +(a-b));
+		System.out.println("Tich cua 2 so a va b la:"+(a*b));
+		System.out.println("Thuong cua 2 so a va b la:"+(a/b));
+
+		}
+	}
